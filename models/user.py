5d01d65c4deb95215b5b7b92a4142c1ccb40fc0f
Obikwelu Chidera <chidexobikwelu@gmail.com>
2024-01-16 18:47:48 +0100

    Update User Class

--- models/user.py
+++ models/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from models.base_model import BaseModel
 from models.base_model import Base
 
