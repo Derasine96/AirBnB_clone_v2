312a8061fb73be8b594fabcf69baa88961a94936
Obikwelu Chidera Jane <chidexobikwelu@gmail.com>
2024-01-31 09:35:05 -0800

    a Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,7 +3,9 @@
 from fabric.api import *
 import os
 
+
 env.hosts = ['100.24.240.5', '100.25.33.28']
+
 
 def do_deploy(archive_path):
     """distributes an archive to your web servers"""
@@ -15,10 +17,14 @@ def do_deploy(archive_path):
         archive_name = os.path.basename(archive_path)
         name_no_ext = os.path.splitext(archive_name)[0]
         run("mkdir -p /data/web_static/releases/{}/".format(name_no_ext))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(archive_name, name_no_ext))
+        cmd = "tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
+            archive_name, name_no_ext)
+        run(cmd)
         run("rm /tmp/{}".format(archive_name))
         run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name_no_ext))
+        cmd = ("ln -s /data/web_static/releases/{}/ "
+               "/data/web_static/current").format(name_no_ext)
+        run(cmd)
         return True
     except Exception as e:
         return False
