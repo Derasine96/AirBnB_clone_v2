bf3afe05a54d6ca5956c1f1fa3beeb3a2718f143
Obikwelu Chidera Jane <chidexobikwelu@gmail.com>
2024-02-01 08:14:45 -0800

    Deploy static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,38 @@
 #!/usr/bin/python3
-""" a Fabric script that distributes an archive to your web servers"""
+"""A Fabric script that distributes an archive to your web servers"""
 from fabric.api import *
 import os
 
-
 env.hosts = ['100.24.240.5', '100.25.33.28']
+env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
-    """distributes an archive to your web servers"""
+    """Distributes an archive to your web servers"""
     if not os.path.exists(archive_path):
+        print("Error: Archive path does not exist.")
         return False
 
     try:
-        put(archive_path, "/tmp/")
         archive_name = os.path.basename(archive_path)
         name_no_ext = os.path.splitext(archive_name)[0]
+
+        put(archive_path, "/tmp/")
         run("mkdir -p /data/web_static/releases/{}/".format(name_no_ext))
-        cmd = "tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
+
+        cmd_extr = "tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
             archive_name, name_no_ext)
-        run(cmd)
+        run(cmd_extr)
+
         run("rm /tmp/{}".format(archive_name))
         run("rm -rf /data/web_static/current")
-        cmd = ("ln -s /data/web_static/releases/{}/ "
-               "/data/web_static/current").format(name_no_ext)
-        run(cmd)
+
+        cmd_link = ("ln -s /data/web_static/releases/{}/ "
+                    "/data/web_static/current").format(name_no_ext)
+        run(cmd_link)
+
+        print("Deployment successful.")
         return True
     except Exception as e:
+        print("Error during deployment:", str(e))
         return False
