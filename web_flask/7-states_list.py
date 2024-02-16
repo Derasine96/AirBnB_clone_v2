032537622066e164c3a73f055b81f9e48c3cf9cc
Obikwelu Chidera Jane <chidexobikwelu@gmail.com>
2024-02-16 14:01:56 +0000

    a flaska app. that retrieves a list of states from a MySQL database

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """method for fetching data from the storage engine"""
+    data = storage.all()
+
+
+@app.teardown_appcontext
+def teardown_db(exception=None):
+    """Define a teardown function to remove the SQLAlchemy session."""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def list_states():
+    """Method to list all states on a database"""
+    states = storage.all("State").values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
+    
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
