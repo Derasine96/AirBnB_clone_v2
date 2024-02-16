64afcb14a3b933fe1675e58a04e26b3016a73e0d
Obikwelu Chidera Jane <chidexobikwelu@gmail.com>
2024-02-16 10:53:09 +0000

    a script that starts a Flask web application

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """A function that displays a text"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def index():
+    """A function that displays a text"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_info(text):
+    """A function that displays a text"""
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_info(text):
+    """A function that displays a text"""
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """A function that displays a number that must be an integer"""
+    return '{:d} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """display a HTML page only if n is an integer"""
+    return render_template('5-number.html', number=int(n))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
